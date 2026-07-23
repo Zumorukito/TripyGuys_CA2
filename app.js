82cbc08ffe966e28f6b5e92c43bde568c75d707d
05junwei18 <25039300@myrp.edu.sg>
2026-07-23 20:58:28 +0800

    ensure dropdown bar works

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const upload = multer({ storage });
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'RP738964$',
+    password: 'JayChou1234@',
     database: 'tripyguys_db'
 });
 
